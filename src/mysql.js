f2505b5c4f7f951824602e74b370b1bb1f5a8da9
Lukas Böttcher <github@muffl0n.de>
2020-05-03 16:00:02 +0200

    Use MySQL Pool-Connection

--- src/mysql.js
+++ src/mysql.js
@@ -1,10 +1,24 @@
-let mysql = require('mysql');
-
-let connection = mysql.createConnection({
-    host    : process.env.DBHOST,
-    user    : process.env.DBUSER,
+var mysql = require('mysql')
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
     password: process.env.DBPASSWORD,
     database: process.env.DBNAME
-});
-
-module.exports = connection;
+})
+pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection was closed.')
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+            console.error('Database has too many connections.')
+        }
+        if (err.code === 'ECONNREFUSED') {
+            console.error('Database connection was refused.')
+        }
+    }
+    if (connection) connection.release()
+    return
+})
+module.exports = pool
